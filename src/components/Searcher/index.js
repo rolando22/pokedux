200720620ef360817e07c17b99344767909d7e4f
rolando22 <rolando2207@gmail.com>
2022-07-21 16:10:24 -0300

    Setup

---
+++ src/components/Searcher/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Input } from "antd";
+
+function Searcher () {
+    return (
+        <Input.Search placeholder="Buscar..." />
+    );
+};
+
+export { Searcher };
